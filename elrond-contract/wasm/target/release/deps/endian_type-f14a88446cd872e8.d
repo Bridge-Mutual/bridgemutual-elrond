7c6c1d095c8fc343ce7d8e8ce384248d4772cf7e
Daelon02 <gubin0202@gmail.com>
2022-04-04 19:25:54 +0300

    Fix script

--- elrond-contract/wasm/target/release/deps/endian_type-f14a88446cd872e8.d
+++
@@ -1,7 +0,0 @@
-/home/daelon/blockchain/bridgemutual-elrond/elrond-contract/wasm/target/release/deps/endian_type-f14a88446cd872e8.rmeta: /home/daelon/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/endian-type-0.1.2/src/lib.rs
-
-/home/daelon/blockchain/bridgemutual-elrond/elrond-contract/wasm/target/release/deps/libendian_type-f14a88446cd872e8.rlib: /home/daelon/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/endian-type-0.1.2/src/lib.rs
-
-/home/daelon/blockchain/bridgemutual-elrond/elrond-contract/wasm/target/release/deps/endian_type-f14a88446cd872e8.d: /home/daelon/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/endian-type-0.1.2/src/lib.rs
-
-/home/daelon/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/endian-type-0.1.2/src/lib.rs:
--- elrond-contract/wasm/target/wasm32-unknown-unknown/release/deps/bmi_token_wasm.d
+++
@@ -1,5 +0,0 @@
-/home/daelon/blockchain/bridgemutual-elrond/elrond-contract/wasm/target/wasm32-unknown-unknown/release/deps/bmi_token_wasm.wasm: src/lib.rs
-
-/home/daelon/blockchain/bridgemutual-elrond/elrond-contract/wasm/target/wasm32-unknown-unknown/release/deps/bmi_token_wasm.d: src/lib.rs
-
-src/lib.rs:
